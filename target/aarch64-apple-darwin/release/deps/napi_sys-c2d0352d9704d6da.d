9788e74f05db6bd185cbfb6b9c7d03f0b3d55e1d
nokhodian <5457777+nokhodian@users.noreply.github.com>
2026-06-02 15:24:01 +0200

    chore: gitignore Rust target/ build cache, untrack committed artifacts

--- target/aarch64-apple-darwin/release/deps/napi_sys-c2d0352d9704d6da.d
+++
@@ -1,9 +0,0 @@
-/Users/morteza/Desktop/tools/monomind/target/aarch64-apple-darwin/release/deps/napi_sys-c2d0352d9704d6da.d: /Users/morteza/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/napi-sys-2.4.0/src/lib.rs /Users/morteza/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/napi-sys-2.4.0/src/functions.rs /Users/morteza/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/napi-sys-2.4.0/src/types.rs
-
-/Users/morteza/Desktop/tools/monomind/target/aarch64-apple-darwin/release/deps/libnapi_sys-c2d0352d9704d6da.rlib: /Users/morteza/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/napi-sys-2.4.0/src/lib.rs /Users/morteza/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/napi-sys-2.4.0/src/functions.rs /Users/morteza/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/napi-sys-2.4.0/src/types.rs
-
-/Users/morteza/Desktop/tools/monomind/target/aarch64-apple-darwin/release/deps/libnapi_sys-c2d0352d9704d6da.rmeta: /Users/morteza/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/napi-sys-2.4.0/src/lib.rs /Users/morteza/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/napi-sys-2.4.0/src/functions.rs /Users/morteza/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/napi-sys-2.4.0/src/types.rs
-
-/Users/morteza/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/napi-sys-2.4.0/src/lib.rs:
-/Users/morteza/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/napi-sys-2.4.0/src/functions.rs:
-/Users/morteza/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/napi-sys-2.4.0/src/types.rs:
--- target/wasm32-unknown-unknown/release/deps/wasm_bindgen_futures-e4fe3270b7d2bc9b.d
+++
@@ -1,7 +0,0 @@
-/Users/morteza/Desktop/tools/monomind/target/wasm32-unknown-unknown/release/deps/wasm_bindgen_futures-e4fe3270b7d2bc9b.d: /Users/morteza/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/wasm-bindgen-futures-0.4.72/src/lib.rs
-
-/Users/morteza/Desktop/tools/monomind/target/wasm32-unknown-unknown/release/deps/libwasm_bindgen_futures-e4fe3270b7d2bc9b.rlib: /Users/morteza/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/wasm-bindgen-futures-0.4.72/src/lib.rs
-
-/Users/morteza/Desktop/tools/monomind/target/wasm32-unknown-unknown/release/deps/libwasm_bindgen_futures-e4fe3270b7d2bc9b.rmeta: /Users/morteza/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/wasm-bindgen-futures-0.4.72/src/lib.rs
-
-/Users/morteza/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/wasm-bindgen-futures-0.4.72/src/lib.rs:
